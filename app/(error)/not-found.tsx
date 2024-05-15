6ed3e5a5c66d555da56062cf510b358ff9339ad6
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-15 23:47:30 +0800

    feat: add 404 page

--- app/(error)/not-found.tsx
+++ app/(error)/not-found.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./error.module.scss";
 
 const Error = () => {
-  return <div className="font-bold text-red-500">Error Cock</div>;
+  return (
+    <div className={styles.error}>
+      <div className={styles.error__face}>
+        <div className={styles.error__face__band}>
+          <div className={styles.error__face__band__red}></div>
+          <div className={styles.error__face__band__white}></div>
+        </div>
+        <div className={styles.error__face__eyes}></div>
+        <div className={styles.error__face__dimples}></div>
+        <div className={styles.error__face__mouth}></div>
+      </div>
+
+      <h1 className="mt-3 text-2xl font-bold text-center  hover:text-blue-500">
+        404 Not Found
+      </h1>
+    </div>
+  );
 };
 
 export default Error;
