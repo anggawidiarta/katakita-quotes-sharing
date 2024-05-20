b7184a07de036aaeb099455c2758921917cf385c
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-20 16:50:02 +0800

    feat: add api route to get all posts

---
+++ app/api/post/route.tsx
@@ -0,0 +1,18 @@
+import { connectToDB } from "@/utils/database";
+import Post from "@/models/post";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request: NextRequest): Promise<NextResponse> => {
+  try {
+    await connectToDB();
+
+    const posts = await Post.find({}).populate("creator");
+    return new NextResponse(JSON.stringify(posts), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return new NextResponse("Failed to fetch all posts", { status: 500 });
+  }
+};
