83b24c84faf362feacde4ca17c370ccc220b9c0a
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-22 23:46:19 +0800

    feat: add profile page components

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import PostCard from "../PostCard/PostCard";
 
-const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+interface ProfileProps {
+  name: string;
+  desc: string;
+  data: never[];
+  handleEdit: () => void;
+  handleDelete: () => void;
+}
+
+const Profile: React.FC<ProfileProps> = ({
+  name,
+  desc,
+  data,
+  handleEdit,
+  handleDelete,
+}) => {
   return (
     <section className="w-full">
-      <h1 className="head_text text-left">{name} Profile</h1>
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} </span>
+        Profile
+      </h1>
+      <p className="text-left">{desc}</p>
+      <div className="mt-16 prompt_layout min-h-[60vh]">
+        {data.map((post: any) => (
+          <PostCard key={post._id} post={post} handleTagClick={handleEdit} />
+        ))}
+      </div>
     </section>
   );
 };
