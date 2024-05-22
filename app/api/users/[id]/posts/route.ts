8ec448a39bf61b890e7ea18799e39e5a6020d147
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-22 13:35:43 +0800

    feat: add route api to get users posts

---
+++ app/api/users/[id]/posts/route.ts
@@ -0,0 +1,23 @@
+import { connectToDB } from "@/utils/database";
+import Post from "@/models/post";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<NextResponse> => {
+  try {
+    await connectToDB();
+
+    const userId = params.id;
+    const posts = await Post.find({ creator: userId }).populate("creator");
+
+    return new NextResponse(JSON.stringify(posts), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return new NextResponse("Failed to fetch all posts", { status: 500 });
+  }
+};
