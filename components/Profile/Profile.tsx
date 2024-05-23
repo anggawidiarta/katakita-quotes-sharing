d8635f08ece8cd513ae27bf3264fa5e890c49459
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-23 14:35:31 +0800

    fix: add more type safety

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import PostCard from "../PostCard/PostCard";
 
+interface Creator {
+  username: string;
+  email: string;
+  image: string;
+}
+
+interface Post {
+  _id: string;
+  creator: Creator;
+  text: string;
+  tag: string;
+}
+
 interface ProfileProps {
   name: string;
   desc: string;
-  data: never[];
+  data: Post[];
   loading: boolean;
-  handleEdit: () => void;
-  handleDelete: () => void;
+  handleEdit: (post: Post) => void;
+  handleDelete: (post: Post) => void;
 }
 
 const Profile: React.FC<ProfileProps> = ({
@@ -25,12 +38,17 @@ const Profile: React.FC<ProfileProps> = ({
         Profile
       </h1>
       <p className="text-left">{desc}</p>
-      <div className="mt-16 prompt_layout min-h-[60vh]">
+      <div className="mt-10 prompt_layout min-h-[60vh]">
         {loading ? (
           <div className="text-2xl font-bold">Loading...</div>
         ) : (
           data.map((post: any) => (
-            <PostCard key={post._id} post={post} handleTagClick={handleEdit} />
+            <PostCard
+              key={post._id}
+              post={post}
+              handleEdit={() => handleEdit && handleEdit(post)}
+              handleDelete={() => handleDelete && handleDelete(post)}
+            />
           ))
         )}
       </div>
