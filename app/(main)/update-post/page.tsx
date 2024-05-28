63af58b28b0c9c56d82dce447b7e1cc08b008b01
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-28 13:06:26 +0800

    style: tidy up import and whitespace

--- app/(main)/update-post/page.tsx
+++ app/(main)/update-post/page.tsx
@@ -4,15 +4,14 @@ import Form from "@/components/Form/Form";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 
 const EditPost: NextPage = () => {
   const router = useRouter();
-  const { data: session } = useSession();
   const searchParams = useSearchParams();
   const postId = searchParams.get("id");
   const [post, setPost] = useState({ text: "", tag: "" });
-  const [loading, setLoading] = useState<boolean>(true);
+
   const [submitting, setSubmitting] = useState<boolean>(false);
 
   useEffect(() => {
