2729e86f147268f4360959b201ac384651331a29
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-28 18:07:42 +0800

    style: change font and other little things

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -8,12 +8,15 @@ export default function Home() {
   const { data: session } = useSession();
   return (
     <section className="flex-col w-full flex-center">
-      <h1 className="text-center head_text">
+      <h1 className="text-center head_text font-playfair">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="text-center orange_gradient"> AI-Powered Prompts</span>
+        <span className="text-center orange_gradient font-playfair">
+          {" "}
+          Prayers, Poem, Quotes for a Better Day
+        </span>
       </h1>
-      <p className="text-center text-black desc">
+      <p className="text-center text-black desc merriweather-light">
         KataKita is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
@@ -21,7 +24,7 @@ export default function Home() {
         <Feed />
       ) : (
         <div>
-          <p className="text-center font-satoshi text-xl font-bold my-52">
+          <p className="text-center merriweather-bold text-xl my-52 capitalize">
             Login in to get started
           </p>
         </div>
