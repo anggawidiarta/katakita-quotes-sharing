c253597dcc9a919944e5d2b399541f11c41d2aa3
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-30 13:36:47 +0800

    style: change color theme

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -9,24 +9,25 @@ export default function Home() {
   const { data: session } = useSession();
   return (
     <section className="flex-col w-full flex-center">
-      <h1 className="text-center head_text font-playfair">
+      <h1 className="text-center head_text text-[#251609] font-playfair">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="text-center orange_gradient font-playfair">
+        <span className="text-center yellow_gradient font-playfair">
           {" "}
           Prayers, Poem, Quotes for a Better Day
         </span>
       </h1>
-      <p className="text-center text-black desc merriweather-light">
-        KataKita is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts
+      <p className="text-center text-[#251609] desc font-montserrat font-medium">
+        {capitalCase(
+          "InspireDaily: Your go-to hub for daily wisdom, uplifting quotes, and heartfelt prayers. Share and discover words that inspire and transform lives."
+        )}
       </p>
       {session?.user ? (
         <Feed />
       ) : (
         <div>
-          <p className="text-center merriweather-bold text-xl my-52 capitalize">
-            Login in to get started
+          <p className="my-48 text-xl text-[#251609] font-semibold text-center capitalize font-montserrat drop-shadow-md">
+            Login to get started
           </p>
         </div>
       )}
