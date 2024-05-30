9f46932d4c71a51ee38c0f911fd81044c73607b4
Angga Widiarta <anggawidiarta55@gmail.com>
2024-05-30 13:39:55 +0800

    style: change font family and theme color

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -43,13 +43,16 @@ const Nav: React.FC = () => {
       {/* Desktop Navigation */}
       <div className="hidden sm:flex">
         {session?.user ? (
-          <div className="flex gap-3 md:gap-5 items-center">
-            <Link href={"/create-post"} className="black_btn">
+          <div className="flex items-center gap-3 md:gap-5">
+            <Link
+              href={"/create-post"}
+              className="black_btn !bg-[#fdd01c] !text-black hover:!bg-black hover:!text-white transition duration-300"
+            >
               Create Post
             </Link>
             <button
               type="button"
-              className="outline_btn"
+              className="transition duration-300 outline_btn"
               onClick={() => signOut()}
             >
               Sign Out
@@ -60,7 +63,7 @@ const Nav: React.FC = () => {
                 src={session?.user.image || "/assets/images/profile.svg"}
                 width={48}
                 height={48}
-                className="rounded-full ring-offset-2 ring-1 ring-violet-500"
+                className="rounded-full ring-offset-4 ring-offset-[#f5fdfc] ring-2 ring-[#fdd01c] hover:ring-[#251609] transition duration-300"
                 alt="profile"
               />
             </Link>
@@ -74,7 +77,7 @@ const Nav: React.FC = () => {
                     type="button"
                     key={provider.name}
                     onClick={() => signIn(provider.id)}
-                    className="black_btn"
+                    className="black_btn !bg-[#fdd01c] !text-black hover:!bg-black hover:!text-white transition duration-300"
                   >
                     Sign In
                   </button>
@@ -90,33 +93,33 @@ const Nav: React.FC = () => {
           <div className="flex">
             <Image
               src={session?.user.image || "/assets/images/profile.svg"}
-              width={37}
-              height={37}
-              className="rounded-full"
+              width={48}
+              height={48}
+              className="rounded-full ring-offset-4 ring-offset-[#f5fdfc] ring-2 ring-[#fdd01c] hover:ring-[#251609] transition duration-300"
               alt="profile"
               onClick={() => {
                 setToggleDropdown((prev) => !prev);
               }}
             />
             {toggleDropdown && (
-              <div className="dropdown">
+              <div className="dropdown !bg-[#251609]">
                 <Link
                   href={"/profile"}
-                  className="dropdown_link"
+                  className="dropdown_link !text-[#f5fdfc]"
                   onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
                   href={"/profile"}
-                  className="dropdown_link"
+                  className="dropdown_link !text-[#f5fdfc]"
                   onClick={() => setToggleDropdown(false)}
                 >
-                  Create Prompt
+                  Create Post
                 </Link>
                 <button
                   type="button"
-                  className="w-full black_btn"
+                  className="w-full black_btn !bg-[#f5fdfc] !text-black hover:!bg-yellow-400 transition-all duration-300 !border-none"
                   onClick={() => signOut()}
                 >
                   Sign Out
@@ -132,7 +135,7 @@ const Nav: React.FC = () => {
                   type="button"
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
-                  className="black_btn"
+                  className="black_btn !bg-[#fdd01c] !text-black hover:!bg-black hover:!text-white transition duration-300"
                 >
                   Sign In
                 </button>
