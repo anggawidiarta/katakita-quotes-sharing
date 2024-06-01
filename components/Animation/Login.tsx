8d9b9f31a386a0cf6b7de825237b1c8b872593f0
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-01 11:45:30 +0800

    chore: declare a login components

---
+++ components/Animation/Login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import styles from "./Login.module.scss";
+// import '@/styles/globals.scss'
+
+const Login = () => {
+  return (
+    <div className="">
+      <div className="monitor-wrapper center">
+        <div className="monitor center">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
