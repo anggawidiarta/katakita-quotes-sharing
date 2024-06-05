41e4c3aa845a63c1b6bba1eb77750a0563348fa6
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-05 17:30:46 +0800

    chore: change metadata content

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,17 +2,40 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.scss";
 import Nav from "@/components/Navbar/Navbar";
-import Feed from "@/components/Feed/Feed";
 import Provider from "@/components/Provider";
 import Footer from "@/components/Footer/Footer";
-import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KataKita",
-  icons: "/assets/icons/bad.png",
+  icons: {
+    icon: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        url: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon-16x16.png",
+      },
+    ],
+  },
   description: "A Powerful Message App",
+  authors: {
+    name: "anggawidiarta",
+    url: "https://github.com/anggawidiarta",
+  },
+  manifest: "/site.webmanifest",
 };
 
 type Props = {
