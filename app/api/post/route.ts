6d4cfa8d00f2a83a6d96ad26c1146da255588812
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-05 08:51:21 +0800

    refactor: give a comment

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -4,11 +4,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
+/**
+ * GET handler to fetch all posts.
+ *
+ * This function connects to the database, retrieves all posts,
+ * populates the "creator" field, and returns the posts in JSON format.
+ * If an error occurs during the process, it logs the error and returns
+ * a 500 status response.
+ *
+ * @param {NextRequest} request - The request object, not used in this handler.
+ *
+ * @returns {Promise<NextResponse>} The response object containing the posts or an error message.
+ */
 export const GET = async (request: NextRequest): Promise<NextResponse> => {
   try {
+    // Connect to the database
     await connectToDB();
 
+    // Retrieve all posts and populate the "creator" field
     const posts = await Post.find({}).populate("creator");
+
+    // Return the posts in JSON format with a 200 status code
     return new NextResponse(JSON.stringify(posts), {
       status: 200,
       headers: {
@@ -16,6 +32,7 @@ export const GET = async (request: NextRequest): Promise<NextResponse> => {
       },
     });
   } catch (error) {
+    // Log the error and return a 500 status response
     console.error("Error fetching posts:", error);
     return new NextResponse("Failed to fetch all posts", { status: 500 });
   }
