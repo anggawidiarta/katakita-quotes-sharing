7975d9a4f866a533038d73fc4890fb21800603e6
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-05 17:31:32 +0800

    fix: add conditions when user not sign in

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PostCard from "../PostCard/PostCard";
 import { capitalCase } from "capital-case";
+import { useSession } from "next-auth/react";
 
 interface Creator {
   username: string;
@@ -32,29 +33,37 @@ const Profile: React.FC<ProfileProps> = ({
   handleEdit,
   handleDelete,
 }) => {
+  const { data: session } = useSession();
+
   return (
     <section className="w-full">
       <h1 className="text-left head_text !merriweather-bold">
         <span className="yellow_gradient">{capitalCase(name)} </span>
         Profile
       </h1>
-      <p className="text-xl tracking-tighter text-[#251609] text-left font-medium capitalize font-montserrat w-3/4">
+      <p className="text-xl tracking-tighter text-[#251609] text-left font-medium capitalize font-montserrat w-full sm:w-3/4">
         {capitalCase(desc)}
       </p>
       <div className="mt-10 prompt_layout min-h-[60vh]">
-        {loading ? (
-          <div>Loading...</div>
-        ) : data.length > 0 ? (
-          data.map((post: any) => (
-            <PostCard
-              key={post._id}
-              post={post}
-              handleEdit={() => handleEdit && handleEdit(post)}
-              handleDelete={() => handleDelete && handleDelete(post)}
-            />
-          ))
+        {session?.user.id ? (
+          loading ? (
+            <div>Loading...</div>
+          ) : data.length > 0 ? (
+            data.map((post: any) => (
+              <PostCard
+                key={post._id}
+                post={post}
+                handleEdit={() => handleEdit && handleEdit(post)}
+                handleDelete={() => handleDelete && handleDelete(post)}
+              />
+            ))
+          ) : (
+            <div className="text-2xl font-bold text-black">No posts</div>
+          )
         ) : (
-          <div className="text-2xl font-bold text-black">No posts</div>
+          <div className="text-lg font-medium text-black w-full">
+            Please Sign In To See The Profile
+          </div>
         )}
       </div>
     </section>
