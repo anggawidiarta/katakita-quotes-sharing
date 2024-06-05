56e2ad80091b43fc29bd267e4834acdfba224d9e
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-06 07:44:21 +0800

    chore: remove inter font

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/styles/globals.scss";
 import Nav from "@/components/Navbar/Navbar";
 import Provider from "@/components/Provider";
 import Footer from "@/components/Footer/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KataKita",
