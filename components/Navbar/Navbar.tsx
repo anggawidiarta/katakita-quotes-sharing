bfa63cc57d2108ab020b44ee63fdeecc16361c76
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-05 17:31:45 +0800

    style: change img in navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -29,16 +29,40 @@ const Nav: React.FC = () => {
 
   return (
     <nav className="w-full mb-16 flex-between">
-      <a href="/" className="flex gap-2 flex-center">
-        <Image
-          src={"/assets/images/comment.png"}
-          alt="Logo Banner"
-          width={50}
-          height={50}
-          className="object-contain"
-        />
-        <p className="logo_text">KataKita</p>
-      </a>
+      <Link href="/" className="flex gap-2 flex-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          width="64"
+          zoomAndPan="magnify"
+          viewBox="0 0 375 374.999991"
+          height="64"
+          preserveAspectRatio="xMidYMid meet"
+          version="1.0"
+        >
+          <defs>
+            <clipPath id="8181b98e37">
+              <path
+                d="M 45.9375 37.5 L 329.4375 37.5 L 329.4375 337.5 L 45.9375 337.5 Z M 45.9375 37.5 "
+                clipRule="nonzero"
+              />
+            </clipPath>
+          </defs>
+          <g clip-path="url(#8181b98e37)">
+            <path
+              fill="#fdd01c"
+              stroke="black"
+              strokeWidth={8}
+              strokeLinejoin="round"
+              paintOrder={"stroke fill markers"}
+              d="M 329.0625 37.550781 C 329.0625 53.253906 316.214844 65.980469 300.632812 65.980469 L 74.367188 65.980469 C 58.785156 65.980469 45.9375 53.253906 45.9375 37.550781 Z M 292.304688 150.207031 L 292.304688 257.746094 C 292.660156 274.519531 282.429688 289.628906 263.992188 303.425781 L 263.992188 259.054688 C 263.992188 258.816406 263.992188 258.578125 263.992188 258.339844 L 263.992188 211.945312 L 224.851562 211.945312 L 224.851562 325.792969 C 216.167969 329.835938 206.773438 333.640625 196.660156 337.449219 L 196.660156 215.160156 L 214.027344 197.789062 L 196.660156 180.421875 L 196.660156 88.703125 L 224.851562 88.703125 L 224.851562 183.632812 L 263.992188 183.632812 L 263.992188 88.703125 L 292.304688 88.703125 Z M 178.339844 150.207031 L 178.339844 180.777344 L 161.328125 197.789062 L 178.339844 214.800781 L 178.339844 337.449219 C 168.226562 333.640625 158.832031 329.835938 150.148438 325.792969 L 150.148438 211.945312 L 111.007812 211.945312 L 111.007812 258.339844 C 111.007812 258.578125 111.007812 258.816406 111.007812 259.054688 L 111.007812 303.425781 C 92.570312 289.628906 82.339844 274.519531 82.695312 257.746094 L 82.695312 88.703125 L 111.007812 88.703125 L 111.007812 183.632812 L 150.148438 183.632812 L 150.148438 88.703125 L 178.339844 88.703125 Z M 178.339844 150.207031 "
+              fillOpacity="1"
+              fillRule="evenodd"
+            />
+          </g>
+        </svg>
+        <p className="logo_text font-playfair tracking-wide">KataKita</p>
+      </Link>
 
       {/* Desktop Navigation */}
       <div className="hidden sm:flex">
@@ -102,7 +126,7 @@ const Nav: React.FC = () => {
               }}
             />
             {toggleDropdown && (
-              <div className="dropdown !bg-[#251609]">
+              <div className="dropdown !bg-[rgba(37,22,9,0.75)]">
                 <Link
                   href={"/profile"}
                   className="dropdown_link !text-[#f5fdfc]"
@@ -111,7 +135,7 @@ const Nav: React.FC = () => {
                   My Profile
                 </Link>
                 <Link
-                  href={"/profile"}
+                  href={"/create-post"}
                   className="dropdown_link !text-[#f5fdfc]"
                   onClick={() => setToggleDropdown(false)}
                 >
